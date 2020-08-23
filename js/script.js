fe5e994a1f3bf1f98c222273199325dec800bbe6
majczel23000 <michal.razny23@gmail.com>
2020-08-23 19:59:22 +0200

    feat(about): about html + css

--- js/script.js
+++ js/script.js
@@ -8,8 +8,15 @@ $(window).on('load', function(){
 // Smooth scrolling to anhor
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
-
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
+});
+
+// Closing mobile menu after click on element
+$(function(){
+    const navMain = $(".navbar-collapse");
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
+    });
 });
