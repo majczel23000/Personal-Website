50cfea3f3fe138617117da729053a1a2e8c90fef
majczel23000 <michal.razny23@gmail.com>
2020-08-23 12:04:54 +0200

    feat(portfolio): portfolio html + css, few examples added, smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
+// Page loader
 $(window).on('load', function(){
     this.setTimeout(() => {
         $('.main-loader').fadeOut();
     }, 500);
  });
+
+// Smooth scrolling to anhor
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
